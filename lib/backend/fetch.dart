c827ebc4abb10b05d1152ffdbfc4c986ce5a5a56
chesnoksatan <chestwins@gmail.com>
2022-07-24 14:45:01 +0300

    fix dahliaOS/files#13

--- lib/backend/fetch.dart
+++ lib/backend/fetch.dart
@@ -45,7 +45,14 @@ class CancelableFsFetch {
     onProgressChange?.call(0.0);
     late final List<FileSystemEntity> list;
     try {
-      list = await directory.list(followLinks: true).toList();
+      list = await directory
+          .list()
+          .where(
+            (element) =>
+                !Utils.getEntityName(element.path).startsWith(".") ||
+                showHidden,
+          )
+          .toList();
     } on FileSystemException catch (e) {
       onFileSystemException?.call(e.osError);
       _cancelled = true;
@@ -60,12 +67,6 @@ class CancelableFsFetch {
         onCancel?.call();
         cancelableCompleter.complete();
         return;
-      }
-
-      String name = Utils.getEntityName(list[i].path);
-      if (name.startsWith(".") && !showHidden) {
-        list.removeAt(i);
-        continue;
       }
 
       late final EntityInfo info;
